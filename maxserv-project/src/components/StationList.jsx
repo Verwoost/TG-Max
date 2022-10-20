bd7c8279822a01327f94d2d0d954f8fc9ef12de2
JoostV <verweij.joost@gmail.com>
2022-10-20 12:05:59 +0200

    add component stationlist

---
+++ maxserv-project/src/components/StationList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function StationList() {
+  return (
+    <ul>
+      <li>
+        <h2>Station Naam </h2> <span> Station Code</span> <span>NL</span>
+      </li>
+    </ul>
+  );
+}
