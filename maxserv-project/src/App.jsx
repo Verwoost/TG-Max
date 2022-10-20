9c70980f5055a880a5818ed5e0d637467c7663e4
JoostV <verweij.joost@gmail.com>
2022-10-20 15:56:52 +0200

    clean app jsx

--- maxserv-project/src/App.jsx
+++ maxserv-project/src/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import StationList from "./components/stationList/StationList";
-import StationGet from "./components/stationList/StationGet";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <StationList></StationList>
-      <StationGet></StationGet>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
