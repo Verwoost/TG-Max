23c10256488c01783546c40496ea8b7f5ba8f66e
JoostV <verweij.joost@gmail.com>
2022-10-20 13:25:28 +0200

    axios and fetch

--- maxserv-project/src/components/StationList.jsx
+++ maxserv-project/src/components/StationList.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import axios from "../axios";
 
 export default function StationList() {
-  return (
-    <ul>
-      <li>
-        <h2>Station Naam </h2> <span> Station Code</span> <span>NL</span>
-      </li>
-    </ul>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get("/stations");
+
+    fetch("https://stoplight.io/mocks/maja/rijsoverzicht/101174922/stations")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={item.stationCode}>
+            {item.name} {item.stationCode} {item.landCode} {facilities}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
